f8842f8914de18a76b300e85bd9ddac339d67eb9
Sorgfalt <yeongjun1324@gmail.com>
2021-07-12 23:27:45 +0900

    Chapter2 변수 연습문제

---
+++ src/variableEx.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class variableEx {
+    public static void main(String[] args) {
+        //숫자를 입력하여 타입이 다른 변수를 형변환하여 두 값을 더하는 프로그램
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("정수형 값을 입력해주세요");
+        String input = sc.nextLine();
+        System.out.printf("실수형 값을 입력해주세요");
+        String input2 = sc.nextLine();
+
+        int num = Integer.parseInt(input);
+        double num2 = Double.parseDouble(input2);
+        int result = num + (int) num2;
+
+        System.out.println("입력내용 : " + result);
+        System.out.printf("num=%d%n", result);
+    }
+}
---
+++ src/variableEx2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class variableEx2 {
+    public static void main(String[] args) {
+        //상수를 이용하여 타입이 다른 값들을 더하고 문자를 입력받아 출력하는 프로그램
+        final double PI = 3.14;
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("첫번째 숫자를 입력해주세요 : ");
+        String input1 = sc.nextLine();
+        System.out.printf("두번째 숫자를 입력해주세요 : ");
+        String input2 = sc.nextLine();
+
+        int num1 = Integer.parseInt(input1);
+        float num2 = Float.parseFloat(input2);
+        int result = num1 *  (int)num2 * (int)PI;
+
+        System.out.printf("문자를 입력해주세요 : ");
+        String text1 = sc.nextLine();
+        String text = text1;
+
+        System.out.printf(text + result);
+    }
+}
