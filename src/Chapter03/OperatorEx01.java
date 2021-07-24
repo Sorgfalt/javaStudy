5333dc2e9f4d1b4f63cf2e583a8a0065f1415f43
Sorgfalt <yeongjun1324@gmail.com>
2021-07-24 12:57:21 +0900

    수정

--- src/Chapter03/OperatorEx01.java
+++ src/Chapter03/OperatorEx01.java
@@ -1,49 +1,48 @@
 package Chapter03;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class OperatorEx01 {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        Scanner initNum = new Scanner(System.in);
-        System.out.println("1번을 누르시면 프로그램이 초기화됩니다");
-        int num = initNum.nextInt();
-        switch (num) {
-            case 1:
-                calculator.init();
-                break;
-        }
+        calculator.init();
+
     }
 }
-class Calculator{
+
+class Calculator {
     Scanner sc = new Scanner(System.in);
     int result;
-    public Calculator(){
+
+    public Calculator() {
 
     }
-    public void init(){
+
+    public void init() {
         int num;
-        System.out.printf("1. 덧셈, 2. 뺄셈, 3. 곱셈, 4.나눗셈 %n");
-        num = sc.nextInt();
 
-        switch (num){
-            case 1:
-                add();
-                break;
-            case 2:
-                sub();
-                break;
-            case 3:
-                mul();
-                break;
-            case 4:
-                div();
-                break;
-        }
+        do {
+            System.out.printf("1. 덧셈, 2. 뺄셈, 3. 곱셈, 4.나눗셈 %n");
+            num = sc.nextInt();
+
+            switch (num) {
+                case 1:
+                    add();
+                    break;
+                case 2:
+                    sub();
+                    break;
+                case 3:
+                    mul();
+                    break;
+                case 4:
+                    div();
+                    break;
+            }
+        } while (num != 4);
     }
 
-    public void add(){
+    public void add() {
         System.out.println("첫번째 숫자를 입력해주세요");
         int num1 = sc.nextInt();
         System.out.println("두번째 숫자를 입력해주세요");
@@ -51,7 +50,8 @@ class Calculator{
         result = num1 + num2;
         System.out.println("결과는 " + result);
     }
-    public void sub(){
+
+    public void sub() {
         System.out.println("첫번째 숫자를 입력해주세요");
         int num1 = sc.nextInt();
         System.out.println("두번째 숫자를 입력해주세요");
@@ -59,7 +59,8 @@ class Calculator{
         result = num1 - num2;
         System.out.println("결과는 " + result);
     }
-    public void mul(){
+
+    public void mul() {
         System.out.println("첫번째 숫자를 입력해주세요");
         int num1 = sc.nextInt();
         System.out.println("두번째 숫자를 입력해주세요");
@@ -67,7 +68,8 @@ class Calculator{
         result = num1 * num2;
         System.out.println("결과는 " + result);
     }
-    public void div(){
+
+    public void div() {
         System.out.println("첫번째 숫자를 입력해주세요");
         int num1 = sc.nextInt();
         System.out.println("두번째 숫자를 입력해주세요");
--- src/Chapter06/PrjMng/productMng/PrjMng.java
+++ src/Chapter06/PrjMng/productMng/PrjMng.java
@@ -2,30 +2,26 @@ package Chapter06.PrjMng.productMng;
 
 import java.util.*;
 
-public class PrjMng{
+public class PrjMng {
     public static void main(String[] args) {
         PrjCrud prjCrud = new PrjCrud();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("1번을 누르시면 프로그램이 초기화됩니다");
-        int num = sc.nextInt();
-        switch (num){
-
-            case 1 : prjCrud.init();
-            break;
-        }
+        prjCrud.init();
     }
 }
 
 class PrjCrud {
     Scanner input = new Scanner(System.in);
+    //menuName란 변수를 ArrayList에 담음
     ArrayList menuName = new ArrayList();
+    //menuPrice란 변수를 ArrayList에 담음
     ArrayList menuPrice = new ArrayList();
 
+    // 생성자 생성
     public PrjCrud() {
 
-    }// 생성자 생성
+    }
 
-    // 초기화
+    // init 생성
     public void init() {
         int num;
 
@@ -51,36 +47,43 @@ class PrjCrud {
 
     public void prdCreate() {
         System.out.println("상품명을 입력해주세요");
+        //next() - 문자 또는 문자열을 공백을 기준으로 한단어 또는 한문자씩 입력
+        //input로 한문자씩 입력 받은 값을 menuName에 추가
         menuName.add(input.next());
         System.out.println("가격을 입력해주세요");
+        //input로 한문자씩 입력 받은 값을 menuPrice 추가 (int)
         menuPrice.add(input.next());
     }
-    public void prdRead(){
+
+    public void prdRead() {
+        //temp 0으로 초기화
         int temp = 0;
         System.out.println("검색할 상품명을 입력해주세요");
         String str = input.next();
+        //indexOf 문자열에 특정 단어가 있는지를 찾고 있을 경우 위치값을 리턴
+        //str에 상품명을 입력한 값이 들어 있고 그 값을 indexOf로 통해 위차 값 리턴
         temp = menuName.indexOf(str);
-        if (temp == 1){
+        if (temp == 1) {
             System.out.println("검색 목록에 조회되지 않습니다");
-        }else {
+        } else {
             System.out.println("검색된 상품입니다");
             System.out.println(menuName.get(temp) + "\t");
             System.out.println(menuPrice.get(temp) + "원" + "\t");
         }
     }
-    public void prdUpdate(){
+
+    public void prdUpdate() {
         String name = " ", str = " ";
         int temp = 0, num = 0;
         System.out.println("수정하고 싶은 상품명을 입력해주세요");
-        input.nextLine();
         name = input.nextLine();
-        temp = menuName.indexOf(menuName);
+        temp = menuName.indexOf(name);
         if (temp == -1) {
             System.out.println("상품명을 찾을 수 없습니다");
-        }else{
+        } else {
             System.out.println("수정하실 부분을 선택해주세요 1. 상품명, 2. 가격");
             num = input.nextInt();
-            switch (num){
+            switch (num) {
                 case 1: {
                     System.out.println("변경하실 상품명을 입력해주세요");
                     input.nextLine();
@@ -102,14 +105,15 @@ class PrjCrud {
             System.out.println(menuPrice.get(temp) + "원" + "\t");
         }
     }
-    public void prdDelete(){
+
+    public void prdDelete() {
         int temp = 0;
         System.out.println("삭제하실 상품명을 입력해주세요");
         String str = input.nextLine();
         temp = menuName.indexOf(str);
-        if (temp == -1){
+        if (temp == -1) {
             System.out.println("검색 목록에서 조회되지 않았습니다");
-        }else{
+        } else {
             menuName.remove(temp);
             menuPrice.remove(temp);
         }
