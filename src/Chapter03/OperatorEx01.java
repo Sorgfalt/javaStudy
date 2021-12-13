4c32323c40844f8f33114ad2d7c91915b1c65792
Sorgfalt <yeongjun1324@gmail.com>
2021-12-13 21:52:17 +0900

    OperatorEx01 Update

--- src/Chapter03/OperatorEx01.java
+++ src/Chapter03/OperatorEx01.java
@@ -4,25 +4,24 @@ import java.util.Scanner;
 
 public class OperatorEx01 {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        calculator.init();
-
+        Calculator calculator = new Calculator(); // Calculator 객체 생성
+        calculator.init(); // calculator라는 변수를 초기화
     }
 }
 
-class Calculator {
-    Scanner sc = new Scanner(System.in);
-    int result;
+class Calculator { // Calculator 클래스를 만듬
+    Scanner sc = new Scanner(System.in); // 스캐너 객체 생성
 
-    public Calculator() {
+    int result; // int형 result 생성
 
-    }
+    int num1; //int형 num1 생성
+    int num2; //int형 num2 생성
 
-    public void init() {
-        int num;
+    public void init() { // int 메소드 생성
+        int num; //int형 num 생성
 
         do {
-            System.out.printf("1. 덧셈, 2. 뺄셈, 3. 곱셈, 4.나눗셈 5. 종료%n");
+            System.out.printf("1. 덧셈, 2. 뺄셈, 3. 곱셈, 4.나눗셈 5. 종료%n"); // 숫자 입력
             num = sc.nextInt();
 
             switch (num) {
@@ -40,43 +39,56 @@ class Calculator {
                     break;
                 case 5:
                     break;
+                default:
             }
         } while (num != 5);
     }
 
-    public void add() {
-        System.out.println("첫번째 숫자를 입력해주세요");
-        int num1 = sc.nextInt();
-        System.out.println("두번째 숫자를 입력해주세요");
-        int num2 = sc.nextInt();
-        result = num1 + num2;
-        System.out.println("결과는 " + result);
+    public void add() { // 1번을 누르면 add 메소드를 실행
+        print1(); // print1 메소드 실행
+        num1 = sc.nextInt(); // num1이라는 변수에 스캐너를 이용하여 값 대입
+        print2(); // print2 메소드 실행
+        num2 = sc.nextInt(); // num2이라는 변수에 스캐너를 이용하여 값 대입
+        result = num1 + num2; // num1과 num2를 더한 값을 result라는 변수에 대입
+        result(); // result 메소드 실행
     }
 
     public void sub() {
-        System.out.println("첫번째 숫자를 입력해주세요");
-        int num1 = sc.nextInt();
-        System.out.println("두번째 숫자를 입력해주세요");
-        int num2 = sc.nextInt();
+        print1();
+        num1 = sc.nextInt();
+        print2();
+        num2 = sc.nextInt();
         result = num1 - num2;
-        System.out.println("결과는 " + result);
+        result();
     }
 
     public void mul() {
-        System.out.println("첫번째 숫자를 입력해주세요");
-        int num1 = sc.nextInt();
-        System.out.println("두번째 숫자를 입력해주세요");
-        int num2 = sc.nextInt();
+        print1();
+        num1 = sc.nextInt();
+        print2();
+        num2 = sc.nextInt();
         result = num1 * num2;
-        System.out.println("결과는 " + result);
+        result();
     }
 
     public void div() {
-        System.out.println("첫번째 숫자를 입력해주세요");
-        int num1 = sc.nextInt();
-        System.out.println("두번째 숫자를 입력해주세요");
-        int num2 = sc.nextInt();
+        print1();
+        num1 = sc.nextInt();
+        print2();
+        num2 = sc.nextInt();
         result = num1 / num2;
+        result();
+    }
+
+    public void print1(){ //print1 메소드
+        System.out.println("첫번째 숫자를 입력해주세요");
+    }
+
+    public void print2(){ // print2 메소드
+        System.out.println("두번째 숫자를 입력해주세요");
+    }
+
+    public void result(){ // result 메소드
         System.out.println("결과는 " + result);
     }
 }
