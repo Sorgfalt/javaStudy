e4e7cb2200d403e0909df056cb5ff2c7665f98a5
Sorgfalt <yeongjun1324@gmail.com>
2022-12-18 17:52:47 +0900

    commit test

--- src/Chapter05/ArrayEx02.java
+++ src/Chapter05/ArrayEx02.java
@@ -3,9 +3,9 @@ package Chapter05;
 import java.util.Scanner;
 
 public class ArrayEx02 {
-    public static void main(String[] args) {
+    public static <Scanner> void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.printf("문자를 입력해주세요 %n");
+        System.out.printf("문자를 입력해주세요123 %n");
         String text1 = sc.nextLine();
         String text2 = sc.nextLine();
         String text3 = sc.nextLine();
